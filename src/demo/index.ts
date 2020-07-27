e33c5130a16d88d4f0eabc642541e32e48a19afa
Devin Don <I.INF@Outlook.com>
2020-07-27 17:21:14 +0000

    feat: update demo

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,12 +1,15 @@
-import { readdirSync, writeFileSync, mkdirSync, mkdir, promises } from 'fs';
+import { existsSync, mkdirSync, readdirSync, writeFileSync } from 'fs';
 import { RecordParser } from '../main';
 
 const allRecords = readdirSync('temp')
   .map(file => ({ file, records: new RecordParser(`temp/${file}`).parse() }));
 
-promises
-  .mkdir('dist')
-  .catch(reason => console.warn(reason))
-  .then(
-    () => writeFileSync('dist/all-records.json', JSON.stringify(allRecords))
-  );
+if (!existsSync('dist')) {
+  mkdirSync('dist');
+}
+
+writeFileSync('dist/all-records.json', JSON.stringify(allRecords));
+
+allRecords.forEach(
+  record => writeFileSync(`dist/${record.file}.json`, JSON.stringify(record.records))
+);
