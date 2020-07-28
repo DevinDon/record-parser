72052f644ba6206732077a6e28808ae1861f2f72
Devin Don <I.INF@Outlook.com>
2020-07-28 09:59:58 +0800

    perf: optimize format

--- src/demo/format.ts
+++ src/demo/format.ts
@@ -13,13 +13,13 @@ const allArchives: Archive[] = JSON.parse(readFileSync('dist/all-archives.json')
 for (const { file, records } of allArchives) {
   const output = records.map(
     record => {
-      let result = `提问用户: ${record.user}\n提问时间：${record.time}\n提问内容：${record.question}\n`;
+      let result = `提问用户：${record.user}\n提问时间：${record.time}\n提问内容：${record.question}\n`;
 
       if (record.answers.length) {
         result = result
           + '回答记录：\n\n'
           + record.answers
-            .map(answer => `- 答疑助教：${answer.user}\n  答疑时间：${answer.time}\n  答疑内容：${answer.content}\n`)
+            .map(answer => `答疑助教：${answer.user}\n答疑时间：${answer.time}\n答疑内容：${answer.content}\n`)
             .join('\n');
       }
 
